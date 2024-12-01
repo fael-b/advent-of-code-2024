c4bcf4e94372c187918ea468daa4cdbaa58fc56f
Fael BASSETTI <fael.bassetti@gmail.com>
2024-12-01 11:31:17 +0100

    day 1 part 2

--- 01/part2.ts
+++ 01/part2.ts
@@ -1,5 +1,19 @@
 import { getInput } from "../lib";
-const inputPath = `${import.meta.dir}/input2.txt`;
+const inputPath = `${import.meta.dir}/input.txt`;
 const input = await getInput(inputPath);
 
-console.log(input.length);
+const lines = input.split("\n");
+const left = [];
+const right = new Map();
+for (const line of lines) {
+  const [a, b] = line.split("   ").map(Number);
+  left.push(a);
+  right.set(b, (right.get(b) ?? 0) + 1);
+}
+
+let similarity = 0;
+for (const num of left) {
+  similarity += (right.get(num) ?? 0) * num;
+}
+
+console.log(similarity);
