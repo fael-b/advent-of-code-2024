caaf73563a4fd7eca5c6cda6ea1b84fea6f5fea8
Fael BASSETTI <fael.bassetti@gmail.com>
2024-12-01 11:22:32 +0100

    day 1 part 1

--- 01/part1.ts
+++ 01/part1.ts
@@ -2,4 +2,22 @@ import { getInput } from "../lib";
 const inputPath = `${import.meta.dir}/input1.txt`;
 const input = await getInput(inputPath);
 
-console.log(input.length);
+const lines = input.split("\n");
+const left = [];
+const right = [];
+for (const line of lines) {
+  const [a, b] = line.split("   ");
+  left.push(Number(a));
+  right.push(Number(b));
+}
+left.sort();
+right.sort();
+
+let sum = 0;
+for (let i = 0; i < left.length; i++) {
+  const a = left[i];
+  const b = right[i];
+  sum += Math.abs(a - b);
+}
+
+console.log(sum);
