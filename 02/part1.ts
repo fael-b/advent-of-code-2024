1a65c04fb8030f5101894568c2796d0747b98a10
Fael BASSETTI <fael.bassetti@pix.fr>
2024-12-02 09:58:50 +0100

    day 2 part 1

--- 02/part1.ts
+++ 02/part1.ts
@@ -2,4 +2,22 @@ import { getInput } from "../lib";
 const inputPath = `${import.meta.dir}/input1.txt`;
 const input = await getInput(inputPath);
 
-console.log(input.length);
+let safeReportsCount = 0;
+for (const line of input.split('\n')) {
+  const levels = line.split(' ').map(Number);
+  const increasing = levels[0] < levels[levels.length - 1];
+  if (!increasing) levels.reverse(); // flemme
+  let isLevelSafe = true;
+  for (let i = 1; i < levels.length; i++) {
+    const previous = levels[i - 1];
+    const current = levels[i];
+    if (!isInBounds(previous, current)) isLevelSafe = false;
+  }
+  if (isLevelSafe) safeReportsCount++;
+}
+
+function isInBounds(previous: number, current: number) {
+  return current >= previous + 1 && current <= previous + 3;
+}
+
+console.log(safeReportsCount);
