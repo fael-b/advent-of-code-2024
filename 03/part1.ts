6b47cc670f2352943b34b363fba7bc56301295ce
Fael BASSETTI <fael.bassetti@pix.fr>
2024-12-03 14:23:19 +0100

    day 3 part 1

--- 03/part1.ts
+++ 03/part1.ts
@@ -1,5 +1,17 @@
 import { getInput } from "../lib";
-const inputPath = `${import.meta.dir}/input1.txt`;
+const inputPath = `${import.meta.dir}/input.txt`;
 const input = await getInput(inputPath);
 
-console.log(input.length);
+let sum = 0;
+
+function mul(a: number, b: number) {
+  sum += a * b;
+}
+
+const mulRegex = /mul\([0-9]{1,3},[0-9]{1,3}\)/g;
+
+for (const [match] of input.matchAll(mulRegex)) {
+  eval(match);
+}
+
+console.log(sum)
