b1e34eec6e357dde40225b486a7b02658173a674
Fael BASSETTI <fael.bassetti@gmail.com>
2024-12-04 21:31:46 +0100

    day 4 part 2

--- 04/part1.ts
+++ 04/part1.ts
@@ -1,5 +1,5 @@
 import { getInput } from "../lib";
-const inputPath = `${import.meta.dir}/input1.txt`;
+const inputPath = `${import.meta.dir}/input.txt`;
 const input = await getInput(inputPath);
 
 let count = 0;
--- 04/part2.ts
+++ 04/part2.ts
@@ -1,5 +1,35 @@
 import { getInput } from "../lib";
-const inputPath = `${import.meta.dir}/input2.txt`;
+const inputPath = `${import.meta.dir}/input.txt`;
 const input = await getInput(inputPath);
 
-console.log(input.length);
+const inputMatrix = input.split("\n").map((line) => line.split(""));
+
+let X_MAS_count = 0;
+
+for (const [i, line] of inputMatrix.entries()) {
+  for (const [j, char] of line.entries()) {
+    if (char !== "A") continue;
+    if (isX_MAS(i, j, inputMatrix)) X_MAS_count++;
+  }
+}
+
+function isX_MAS(i: number, j: number, matrix: string[][]) {
+  const topLeft = matrix?.[i - 1]?.[j - 1];
+  const topRight = matrix?.[i - 1]?.[j + 1];
+  const bottomRight = matrix?.[i + 1]?.[j + 1];
+  const bottomLeft = matrix?.[i + 1]?.[j - 1];
+
+  const hasFirstMAS = isMAS(topLeft, bottomRight);
+  const hasSecondMAS = isMAS(topRight, bottomLeft);
+
+  return hasFirstMAS && hasSecondMAS;
+}
+
+function isMAS(first: string, second: string) {
+  const MS = new Set(["M", "S"]);
+  if (!MS.has(first)) return false;
+  MS.delete(first);
+  return MS.has(second);
+}
+
+console.log(X_MAS_count);
